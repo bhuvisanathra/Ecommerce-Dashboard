954faa34b2278ed6605d6366c0ab45c58177d552
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-05-07 10:58:54 +0530

    Backend- Mongoose Connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
-//Use to make APIs
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+//MongoDb Connection
+const mongoose = require('mongoose');
+
+//ConnectDb Function
+const connectDb = async () => {
+    mongoose.connect("mongodb://localhost:27017/ecomm");
+    //Schema
+    const productSchema = new mongoose.Schema({});
+    //Model : Which will only fetch the required fields
+    const product = mongoose.model('product', productSchema);
+    const data = await product.find();
+    console.log(data);
+}
+
+connectDb();
 
 app.listen(5000, () => {
     console.log("Server is running at http://localhost:5000");
