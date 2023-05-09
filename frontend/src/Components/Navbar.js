f33cbe3bf0ac9a16248fecd17fecb0f8f722522a
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-05-09 19:59:46 +0530

    SignUp Fixed!!
    Navigate Bug Removed &
    Logout Clear Storage Done

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const Navbar = () => {
     const auth = localStorage.getItem('users');
+    const Navigate = useNavigate();
+
+    //Logout Function
+    //Remove data from Local Storage
+    const logout = () => {
+        localStorage.clear();
+        Navigate("/SignUp");
+    }
     return (
         <div>
             <ul className='nav-ul'>
@@ -10,7 +18,7 @@ const Navbar = () => {
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/remove">Remove Products</Link></li>
                 <li><Link to="/Profile">My Profile</Link></li>
-                <li> {auth ? <Link to="/logout">Logout</Link> : <Link to="/Signup">Sign Up</Link>}</li>
+                <li> {auth ? <Link onClick={logout} to="/logout">Logout</Link> : <Link to="/Signup">Sign Up</Link>}</li>
             </ul>
         </div>
     )
