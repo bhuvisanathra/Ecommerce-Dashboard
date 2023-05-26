d9af5b9571ab7c71ee98cab37e98b398f3cda04d
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-05-26 10:02:46 +0530

    Navbar Module Updated

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -12,22 +12,26 @@ const Navbar = () => {
     }
     return (
         <div>
-            <ul className='nav-ul'>
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/add">Add Products</Link></li>
-                <li><Link to="/update">Update Products</Link></li>
-                <li><Link to="/remove">Remove Products</Link></li>
-                <li><Link to="/Profile">My Profile</Link></li>
-                {
-                    auth ?
-                        <li><Link onClick={logout} to="/Signup">Logout</Link></li>
-                        : <>
-                            <li><Link to="/Signup">Sign Up</Link></li>
-                            <li><Link to="/Login">Login</Link></li>
-                        </>
-
-                }
-            </ul>
+            <img
+                alt='Logo'
+                className='logoImg'
+                src='https://www.adobe.com/express/create/logo/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium'
+            />
+            {
+                auth ?
+                    <ul className='nav-ul'>
+                        <li><Link to="/">Products</Link></li>
+                        <li><Link to="/add">Add Products</Link></li>
+                        <li><Link to="/update">Update Products</Link></li>
+                        <li><Link to="/remove">Remove Products</Link></li>
+                        <li><Link to="/Profile">My Profile</Link></li>
+                        <li><Link onClick={logout} to="/Signup">Logout ({JSON.parse(auth).name})</Link></li>
+                    </ul>
+                    : <ul className='nav-ul'>
+                        <li><Link to="/Signup">Sign Up</Link></li>
+                        <li><Link to="/Login">Login</Link></li>
+                    </ul>
+            }
         </div>
     )
 }
